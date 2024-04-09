3cd755d7460a16e0bfc657059335c93c29a67997
Joao <joaovictorsantos10@hotmail.com.br>
2024-04-09 14:09:22 -0300

    .

--- src/Questao08.java
+++ src/Questao08.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Questao08 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
+
+        in.close();
+    }
 }
