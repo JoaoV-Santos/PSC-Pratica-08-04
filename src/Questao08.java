77e1fcf02c7acadd0dbbe5ab7cace126743d9819
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-09 09:28:41 -0300

    questao 08 resolvida

--- src/Questao08.java
+++ src/Questao08.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Questao08 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Informe o valor de N: ");
+        double n = in.nextDouble();
+        double h = 0;
+
+        for (int i = 1; i <= n; i++) {
+            h += 1.0 / i;
+        }
+
+        System.out.println("H = " + h);
+
+
+        in.close();
+    }
 }
